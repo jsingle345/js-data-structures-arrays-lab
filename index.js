b2b944cb409ba3689eeaa5557b4536bae0018e05
jsingle345 <jsingle345@gmail.com>
2020-11-01 10:06:29 -0600

    commit tree 9df7a94418c378145759fd1a0e8a58658ec21a47

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+
+
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+};
+
+destructivelyPrependDriver = (name) =>{
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop();
+}
+
+// destructivelyRemoveFirstDriver = () => {
+//     drivers.shift();
+// }
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver(name){
+    const newDrivers = [...drivers, name]
+    return newDrivers
+}
+
+function prependDriver(name){
+    const newDrivers = [name, ...drivers]
+    return newDrivers
+}
+
+function removeFirstDriver(){
+   return drivers.slice(1);
+}
+
+function removeFirstDriver () {
+    return drivers.slice(1);
+  }
+
+function removeLastDriver(){
+    return drivers.slice(0, -1)
+}
